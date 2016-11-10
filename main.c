696e37d4e1eaf907275b6d5589baac5998c9eb72
Satyamjay <Satyamisgod@gmail.com>
2016-11-10 16:09:41 +0530

    odd even program
    without modulus or binary conversion

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int  a,c;
+float b,d;
+printf("enter the no.");
+scanf("%d",&a);
+b=a;
+c=a/2;
+d=b/2;
+if (c==d)
+printf("even");
+else
+printf("odd");
+getch();
+}
